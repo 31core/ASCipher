4eecd8b5451cee5c53e02c97c43deb8367ed6c36
31core <31core@tutanota.com>
2023-10-21 19:46:35 +0800

    Update utils/asc-hash/src/main.rs

--- utils/asc-hash/src/main.rs
+++ utils/asc-hash/src/main.rs
@@ -1,11 +1,28 @@
 use ascipher::hash::*;
 use std::io::Read;
 
-const BUFFER_SIZE: usize = 54 * 1024;
+const BUFFER_SIZE: usize = 54 * 4096;
 
-fn main() -> std::io::Result<()> {
-    let args = std::env::args().collect::<Vec<String>>();
-    let mut input_f = std::fs::File::open(&args[1])?;
+fn hash_stdin() -> std::io::Result<()> {
+    let mut hasher = Hasher512::default();
+    loop {
+        let mut buffer = [0; BUFFER_SIZE];
+        let len = std::io::stdin().read(&mut buffer)?;
+        hasher.update(&buffer[..len]);
+        if len == 0 {
+            break;
+        }
+    }
+    let hash_sum = hasher.digest();
+    for i in hash_sum {
+        print!("{:02x}", i);
+    }
+    println!("  -");
+    Ok(())
+}
+
+fn hash_file(path: &str) -> std::io::Result<()> {
+    let mut input_f = std::fs::File::open(path)?;
     let mut hasher = Hasher512::default();
     loop {
         let mut buffer = [0; BUFFER_SIZE];
@@ -17,8 +34,20 @@ fn main() -> std::io::Result<()> {
     }
     let hash_sum = hasher.digest();
     for i in hash_sum {
-        print!("{:x}", i);
+        print!("{:02x}", i);
     }
-    println!();
+    println!("  {}", path);
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let args = std::env::args().collect::<Vec<String>>();
+    if args.len() == 1 {
+        hash_stdin()?;
+    } else {
+        for file in &args[1..] {
+            hash_file(file)?;
+        }
+    }
     Ok(())
 }
